1111925a35f23c40a90b9fab9c1caf4d1bf9d1e9
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-01-15 10:11:00 +0600

    files added

---
+++ parent/feedback.php
@@ -0,0 +1,38 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parent</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <!--==================== Wels ====================-->
+        <div class="jumbotron">
+            <h1>Android Attendance System</h1>
+        </div>
+        
+        <!--==================== Navigation ====================-->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Add Feedback</a></li>
+                    <li><a href="#">Student Report</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
---
+++ parent/index.php
@@ -0,0 +1,2 @@
+<?php
+header('location: feedback.php');
