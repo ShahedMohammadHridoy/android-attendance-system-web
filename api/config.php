b7a8e146ab16349ff27c8a189c5bdd19cc023291
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-01-15 10:15:39 +0600

    config file updated

--- api/config.php
+++ api/config.php
@@ -1,8 +1,15 @@
 <?php
 
-//configuration
-$SERVER = 'localhost';
-$USER = 'root';
-$PASS = '';
-$DB = '';
+//DATABASE
+$SERVER = "localhost";
+$USER = "root";
+$PASS = "";
+$DB = "aas";
 
+//Connection
+$mysqli = new mysqli($SERVER, $USER, $PASS, $DB);
+
+//Check Connection
+if($mysqli->connect_errno){
+    echo 'Failed to connect database:'.$mysqli->connect_error;
+}
